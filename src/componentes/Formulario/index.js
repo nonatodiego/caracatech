0c29b16a2a75facb6627de650e5969d4223519b8
Diego Nonato <nonatodiego2014@gmail.com>
2024-06-29 11:19:45 -0300

    feature: criando botao do form

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,14 +1,21 @@
 import './Formulario.css';
 import CampoTexto from '../CampoTexto'; 
+import ListaSuspensa from '../ListaSuspensa';
+import Botao from '../Botao';
 
 const Formulario = () => {
+    const times = ['Peladeiros', 'Galácticos', 'Geração Nutella', 'Brabos', 'Mitos'];
     return (
         <section className='formulario'>
             <form>
-                <h2>Preencha os dados para criar o card do carioca</h2>
-                <CampoTexto label='Apelido' placeholder='Digite seu apelido' />
-                <CampoTexto label='Profissa' placeholder='Digite sua profissa' />
+                <h2>Preencha os dados do jogador</h2>
+                <CampoTexto label='Nome' placeholder='Digite seu nome' />
+                <CampoTexto label='Posição' placeholder='Digite sua posição' />
                 <CampoTexto label='Imagem' placeholder='Digite o endereço da imagem' />
+                <ListaSuspensa itens={times} label='Time' />
+                <Botao> 
+                    Criar Card
+                </Botao>
             </form>
         </section>
     )
